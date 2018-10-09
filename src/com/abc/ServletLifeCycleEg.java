95feae13291fdb92246571b078eda70c83f4512d
unknown <vskreddy652@yahoo.com>
2018-10-09 11:04:48 +0530

    This is first commit

---
+++ src/com/abc/ServletLifeCycleEg.java
@@ -0,0 +1,55 @@
+package com.abc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLifeCycleEg
+ */
+@WebServlet("/ServletLifeCycleEg")
+public class ServletLifeCycleEg extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@Override
+	public void init(ServletConfig sconfig)
+	{
+		System.out.println("This is servlet init");
+	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLifeCycleEg() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doget.......");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	@Override
+	public void destroy()
+	{
+		System.out.println("This is detsroy method.........");
+	}
+
+}
